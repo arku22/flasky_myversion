a0b4d826c1fb5cd4a9efb0ba396b36f47a3631e4
Archit Kumar <archit@wpi.edu>
2022-05-09 21:22:47 -0500

    Create application in flasky.py file

---
+++ flasky.py
@@ -0,0 +1,13 @@
+from app import create_app, db
+from flask_migrate import Migrate
+from app.models import User, Role
+
+
+app = create_app('default')
+migrate = Migrate(app, db)
+
+
+# add objects to be imported by default
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
