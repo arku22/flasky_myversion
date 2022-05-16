027901810d1d6dc805105bc2cb85c2cf4887b79d
Archit Kumar <archit@wpi.edu>
2022-05-15 19:18:32 -0500

    'auth' blueprint: Move 'email' column to User table

--- app/models.py
+++ app/models.py
@@ -5,23 +5,23 @@ from . import login_manager
 
 
 # define database tables
-class Role(UserMixin, db.Model):
+class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     rolename = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
-    email = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return f"{self.name}"
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), unique=True, index=True)
 
     @property
     def password(self):
