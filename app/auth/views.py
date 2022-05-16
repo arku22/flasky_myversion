b3e631eb00ebd5009bb4754403f8228ea734c6d1
Archit Kumar <archit@wpi.edu>
2022-05-16 09:08:05 -0500

    'auth' blueprint: Add functionality to register new user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from . import auth
 from flask import render_template, flash, request, url_for, redirect
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 from flask_login import login_user, login_required, logout_user
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -27,3 +28,17 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data
+                    )
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
