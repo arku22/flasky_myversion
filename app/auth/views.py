8362dad6f0852236f8d4d54ba5edf96350704de5
Archit Kumar <archit@wpi.edu>
2022-05-17 20:54:47 -0500

    Add functionality to allow user to update account password

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from . import auth
 from flask import render_template, flash, request, url_for, redirect
-from .forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm
 from ..models import User
 from flask_login import login_user, login_required, logout_user, current_user
 from .. import db
@@ -94,3 +94,18 @@ def resend_confirmation():
     flash('A new confirmation email has been sent to you by email.')
     return redirect(url_for('main.index'))
 
+
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid password.')
+    return render_template("auth/change_password.html", form=form)
